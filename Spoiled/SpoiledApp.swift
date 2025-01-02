2fc92d23141abc69add5748f4590b427ca9dce75
Tommy Ledford <tledford58@gmail.com>
2025-01-01 20:50:00 -0800

    Initial Commit

---
+++ Spoiled/SpoiledApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoiledApp.swift
+//  Spoiled
+//
+//  Created by Tommy Ledford on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SpoiledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpoiledTests/SpoiledTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpoiledTests.swift
+//  SpoiledTests
+//
+//  Created by Tommy Ledford on 1/1/25.
+//
+
+import Testing
+@testable import Spoiled
+
+struct SpoiledTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
